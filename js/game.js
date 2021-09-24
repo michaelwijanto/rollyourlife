3eb46b2e41bb1d4d84dad04fbe589c3fb2e0732b
michaelwijanto <michael.wijanto@gmail.com>
2021-09-24 12:56:56 +0700

    more fixed + updates

--- js/game.js
+++ js/game.js
@@ -7,13 +7,13 @@ function dokter(player) {
     return balanceHabis(player);
   }
   player.balance -= 2000;
-  player.lastEvent = `Anda sakit, jadi anda terpaksa ke dokter.\n Balance anda berkurang sebanyak 2000.\n Sisa balance anda ${player.balance}`;
+  player.lastEvent = `Anda sakit, jadi anda terpaksa ke dokter.<br> Balance anda berkurang sebanyak 2000.\n Sisa balance anda ${player.balance}`;
   return player;
 }
 
 function bansos(player) {
   player.balance += 1000;
-  player.lastEvent = `Bantuan pemerintah telah beredar. Anda mendapatkan 1000.\n Balance anda sekarang ${player.balance}`;
+  player.lastEvent = `Bantuan pemerintah telah beredar. Anda mendapatkan 1000.<br> Balance anda sekarang ${player.balance}`;
   return player;
 }
 
@@ -25,10 +25,10 @@ function investasi(player) {
   let string = `Anda mendapatkan tawaran untuk berinvestasi. Anda merasa beruntung jadi anda memutuskan untuk invest.`;
   if (player.diceOption > 6) {
     player.balance += player.balance * 0.2;
-    string += `\n Perasaan anda tepat! Investasi anda berbuah sebesar 20%. Balance anda sekarang ${player.balance}`;
+    string += `<br> Perasaan anda tepat! Investasi anda berbuah sebesar 20%. <br>Balance anda sekarang ${player.balance}`;
   } else {
     player.balance -= player.balance * 0.2;
-    string += `\n Sayangnya perasaan anda kurang tepat. Balance anda sekarang ${player.balance}`;
+    string += `<br> Sayangnya perasaan anda kurang tepat. Anda rugi sebesar 20% <br> Balance anda sekarang ${player.balance}`;
   }
   player.lastEvent = string;
   return player;
@@ -39,19 +39,19 @@ function nikah(player) {
     return balanceHabis(player);
   }
   player.balance -= 4000;
-  player.lastEvent = `Selain didesak oleh kekasih anda, anda merasa hari ini merupakan hari baik untuk menikah. Balance anda berkurang 4000. Sisa balance anda ${player.balance}`;
+  player.lastEvent = `Selain didesak oleh kekasih anda, anda merasa hari ini merupakan hari baik untuk menikah. Balance anda berkurang 4000.<br> Sisa balance anda ${player.balance}`;
   return player;
 }
 
 function istriHamil(player) {
   player.balance += 2500;
-  player.lastEvent = `Kabar gembira untuk anda! Keluarga anda mulai sekarang akan bertambah 1. Teman-teman anda ikut bergembira dan memberi anda hadiah dini sebesar 2500.\n Balance anda sekarang ${player.balance}`;
+  player.lastEvent = `Kabar gembira untuk anda! Keluarga anda mulai sekarang akan bertambah 1. Teman-teman anda ikut bergembira dan memberi anda hadiah dini sebesar 2500.<br> Balance anda sekarang ${player.balance}`;
   return player;
 }
 
 function naikJabat(player) {
   player.balance += 4000;
-  player.lastEvent = `Anda mendapat kabar bahwa anda telah dinaikan jabatannya. Balance bertambah 4000.\n Balance anda sekarang ${player.balance}`;
+  player.lastEvent = `Anda mendapat kabar bahwa anda telah dinaikan jabatannya. Balance bertambah 4000.<br> Balance anda sekarang ${player.balance}`;
   return player;
 }
 
@@ -60,20 +60,20 @@ function tetanggaHamil(player) {
     return balanceHabis(player);
   }
   player.balance -= 2500;
-  player.lastEvent = `Kabar gembira untuk tetangga anda! Istri tetangga hamil sehingga mereka berpesta. Anda mengikuti pesta mereka dan memberi hadiah dini sebesar 2500. Balance anda sekarang ${player.balance}`;
+  player.lastEvent = `Kabar gembira untuk tetangga anda! Istri tetangga hamil sehingga mereka berpesta. Anda mengikuti pesta mereka dan memberi hadiah dini sebesar 2500.<br> Balance anda sekarang ${player.balance}`;
   return player;
 }
 
 function hacktiv8(player) {
   if (player.diceOption > 4) {
     player.balance += 6000;
-    player.lastEvent = `Anda memutuskan untuk mengikuti hacktiv8. Setelah membayar dan mengikuti kelas dengan giat, anda lulus dan mendapatkan pekerjaan tambahan. Balance anda bertambah 6000.\n Balance anda sekarnag ${player.balance}`;
+    player.lastEvent = `Anda memutuskan untuk mengikuti hacktiv8. Setelah membayar dan mengikuti kelas dengan giat, anda lulus dan mendapatkan pekerjaan tambahan. Balance anda bertambah 6000.<br> Balance anda sekarnag ${player.balance}`;
   } else {
     if (player.balance == 0) {
       return balanceHabis(player);
     }
     player.balance -= 6000;
-    player.lastEvent = `Anda memutuskan untuk mengikuti hacktiv8. Setelah membayar anda mengikuti kelas dengan giat.\n Namun sayangnya anda gugur dalam fase pembelajaran. Balance anda berkurang 6000.\n Balance anda sekarang ${player.balance}`;
+    player.lastEvent = `Anda memutuskan untuk mengikuti hacktiv8. Setelah membayar anda mengikuti kelas dengan giat. Namun sayangnya anda gugur dalam fase pembelajaran. Balance anda berkurang 6000.<br> Balance anda sekarang ${player.balance}`;
   }
   return player;
 }
@@ -212,15 +212,16 @@ let player = {
 //DOM
 let rollButton = document.getElementById("roll");
 let paragraph = document.getElementById("test");
-let counter = document.getElementById("counter");
 let actions = document.getElementById("actions");
 let resetButton = document.getElementById("reset");
+let wallet = document.getElementById("wallet");
+let dadu = document.getElementById("dadu");
+let avatarimg = document.getElementById("avatarimg");
 let count = 1;
+
 rollButton.addEventListener("click", () => {
   console.log(`clicked`);
-  // console.log(player);
   player = rollTheDice(player);
-  // console.log(player.dice);
   let i = player.history.length - 1;
 
   if (player.history[i].balanceThen == player.history[i].balanceNow) {
@@ -232,16 +233,46 @@ rollButton.addEventListener("click", () => {
     rollButton.innerText = "GAME OVER";
     actions.innerHTML += "<br>" + result(player.balance, 10000);
   }
-  counter.value = count++;
-  paragraph.innerHTML += `Dadu: ${player.history[i].dice} || Balance: ${player.history[i].balanceThen} --> ${player.history[i].balanceNow} || Action: ${player.history[i].action}<br>`;
+
+  wallet.innerText = "Rp " + player.balance;
+  if (player.balance < 0) {
+    wallet.style = " color: red;";
+    avatarimg.src = "./Assets/Images/deadAvatar.png";
+  } else if (player.balance < 10000) {
+    wallet.style = " color: orangered;";
+    avatarimg.src = "./Assets/Images/poorAvatar.png";
+  } else {
+    if (player.balance > 17000) {
+      avatarimg.src = "./Assets/Images/richAvatar.png";
+    } else avatarimg.src = "./Assets/Images/flatAvatar.png";
+    wallet.style = " color:black;";
+  }
+  dadu.innerText = player.dice;
+  paragraph.innerHTML += `Dadu: ${player.history[i].dice}<br> Balance: ${player.history[i].balanceThen} --> ${player.history[i].balanceNow}<br>Action: ${player.history[i].action}<br>----------------------------------------------------------<br>`;
   actions.innerHTML = player.lastEvent;
+
+  let pindahBulat = document.getElementById(`bulat-${count}`);
+  let gantiAktif = document.getElementById(`bulat-${count - 1}`);
+  gantiAktif.className = "bulat dead";
+  pindahBulat.className = "bulat active";
+  count++;
 });
 
 resetButton.addEventListener("click", () => {
   count = 1;
   paragraph.innerHTML = "";
-  counter.value = 0;
-  actions.innerHTML = "NEW GAME";
+  avatarimg.src = "./Assets/Images/flatAvatar.png";
+  actions.innerHTML = "";
+  rollButton.disabled = false;
+  rollButton.innerText = "ROLL DADU";
+  wallet.innerText = "Rp " + 10000;
+  dadu.innerText = 0;
+  wallet.style = "color:black";
+  for (let i = 0; i < 11; i++) {
+    let temp = document.getElementById(`bulat-${i}`);
+    if (i == 0) temp.className = "bulat active";
+    else temp.className = "bulat";
+  }
   player = {
     name: "bejo",
     balance: 10000,
